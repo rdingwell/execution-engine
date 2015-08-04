5459a592f9c29649ebe1c550ded1017543ff5fc2
Andrew Hubley <ahubley@mitre.org>
2015-08-04 14:03:36 -0400

    added cql library manager to resolve cql library based on url

--- routes/execute.js
+++ routes/execute.js
@@ -1,30 +1,13 @@
 var express = require('express');
 var cql = require('cql-execution');
 var measure = require('cql-execution/lib/example/age')
+LibraryManager = require("cql-library-manager").LibraryManager
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  var lib = new cql.Library(measure);
-  psource = new cql.PatientSource([ {
-                            "resourceType": "Bundle",
-                            "id": "example1",
-                            "meta": {
-                              "versionId": "1",
-                              "lastUpdated": "2014-08-18T01:43:30Z"
-                            },
-                            "base": "http://example.com/base",
-                            "entry" : [{
-                                  "resource": {
-                                  "id" : "1",
-                                  "meta" :{ "profile" : ["patient-qicore-qicore-patient"]},
-                                  "resourceType" : "Patient",
-                                  "identifier": [{ "value": "1" }],
-                                  "name": {"given":["John"], "family": ["Smith"]},
-                                  "gender": "M",
-                                  "birthDate" : "1980-02-17T06:15"}
-                                  }
-                            ]
-                          } ]);
+router.post('/:library/:version', function(req, res, next) {
+  manager = new LibraryManager("./cql_libraries");
+  var lib = manager.resolve(req.params.library, req.params.version);
+  psource = new cql.PatientSource(req.body);
   executor = new cql.Executor(lib);
   result = executor.exec(psource);
   res.send(JSON.stringify(result, undefined, 2))
