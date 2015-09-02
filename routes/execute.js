e229641cd26f9d1ca8ea760abd38bb5b7cbb0236
Rob Dingwell <bobd@mitre.org>
2015-09-02 13:20:11 -0400

    adding everest patient source. Yes this needs to go somewhere else but I'm in a rush

--- routes/execute.js
+++ routes/execute.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var cql = require('cql-execution');
+var ps = require('../lib/patient_source')
 var measure = require('cql-execution/lib/example/age')
 LibraryManager = require("cql-library-manager").LibraryManager
 var router = express.Router();
-
+console.log(require('../lib/patient_source'))
 router.post('/:library/:version', function(req, res, next) {
+  
   manager = new LibraryManager("./cql_libraries");
   var lib = manager.resolve(req.params.library, req.params.version);
-  psource = new cql.PatientSource(req.body);
+  psource = new cql.PatientSource([req.body]);
   executor = new cql.Executor(lib);
   result = executor.exec(psource);
   res.send(JSON.stringify(result, undefined, 2))
