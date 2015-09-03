1261c5fb393ff8250cbcf31d8afb5438b9f05a9a
Rob Dingwell <rob@robdingwell.org>
2015-09-03 18:24:59 -0400

    updating patient model to match new everest model.  Updating everest library and cql execution engine version.  Removed cql-library-manager from project, it was causing odd type checking issues where DateTime appeared to be loaded more than once in different locations and depending on where the DateTime was created and where instanceOf DateTime was called would create inconsistent results

--- routes/execute.js
+++ routes/execute.js
@@ -2,39 +2,13 @@ var express = require('express');
 var cql = require('cql-execution');
 var ps = require('../lib/patient_source')
 var measure = require('cql-execution/lib/example/age')
-LibraryManager = require("cql-library-manager").LibraryManager
+LibraryManager = require("../lib/library_manager").LibraryManager
 var router = express.Router();
-console.log(require('../lib/patient_source'))
+
 router.post('/:library/:version', function(req, res, next) {
-  debugger;
   manager = new LibraryManager("./cql_libraries");
   var lib = manager.resolve(req.params.library, req.params.version);
-  console.log(req)
-  psource = new ps.PatientSource([{"name":"ted", "service":[{
-  "EOD": "2000-01-01",
-  "RAD": "2000-01-01",
-  "character": "",
-  "separationReason": "",
-  "training":[
-    {"STD": "2000-01-01",
-    "EDT": "2000-01-01"},
-     {"STD": "2000-01-01",
-    "EDT": "2000-01-01"}
-    ]
-},
-{
-  "EOD": "2000-01-01",
-  "RAD": "2000-01-01",
-  "character": "",
-  "separationReason": "",
-  "training":[
-    {"STD": "2000-01-01",
-    "EDT": "2000-01-01"},
-     {"STD": "2000-01-01",
-    "EDT": "2000-01-01"}
-    ]
-}
-]}]);
+  psource = new ps.PatientSource([req.body]);
   executor = new cql.Executor(lib);
   result = executor.exec(psource);
   res.send(JSON.stringify(result, undefined, 2))
