fb921432aa55a78bbd50cbd2ee258c060c642a7d
Rob Dingwell <bobd@mitre.org>
2015-09-03 07:15:37 -0400

    attempting to inegrate with the everest data model

--- routes/execute.js
+++ routes/execute.js
@@ -6,10 +6,35 @@ LibraryManager = require("cql-library-manager").LibraryManager
 var router = express.Router();
 console.log(require('../lib/patient_source'))
 router.post('/:library/:version', function(req, res, next) {
-  
+  debugger;
   manager = new LibraryManager("./cql_libraries");
   var lib = manager.resolve(req.params.library, req.params.version);
-  psource = new cql.PatientSource([req.body]);
+  console.log(req)
+  psource = new ps.PatientSource([{"name":"ted", "service":[{
+  "EOD": "2000-01-01",
+  "RAD": "2000-01-01",
+  "character": "",
+  "separationReason": "",
+  "training":[
+    {"STD": "2000-01-01",
+    "EDT": "2000-01-01"},
+     {"STD": "2000-01-01",
+    "EDT": "2000-01-01"}
+    ]
+},
+{
+  "EOD": "2000-01-01",
+  "RAD": "2000-01-01",
+  "character": "",
+  "separationReason": "",
+  "training":[
+    {"STD": "2000-01-01",
+    "EDT": "2000-01-01"},
+     {"STD": "2000-01-01",
+    "EDT": "2000-01-01"}
+    ]
+}
+]}]);
   executor = new cql.Executor(lib);
   result = executor.exec(psource);
   res.send(JSON.stringify(result, undefined, 2))
